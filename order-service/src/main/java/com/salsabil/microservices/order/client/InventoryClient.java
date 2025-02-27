0c303331012ae48f1c4ace0331caa9641783be48
salsabilk <salsabilkhoirunisa271@gmail.com>
2025-02-27 14:53:04 +0700

    part 9 dan 10 done

--- order-service/src/main/java/com/salsabil/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/salsabil/microservices/order/client/InventoryClient.java
@@ -1,11 +1,26 @@
 package com.salsabil.microservices.order.client;
 
-import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.GetExchange;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+// import lombok.extern.slf4j.Slf4j;
+
+// @Slf4j
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+    
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason {}", code, throwable.getMessage());
+        return false;
+    }
 }
